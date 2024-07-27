fa7536355ffbae296bf2dde9df0bf0523c8b58a2
AlexBoronin <alex.boronin.22@gmail.com>
2024-07-27 17:30:40 +0300

    old logo

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,16 @@
 new WOW({
   animateClass: 'animate__animated',
 }).init();
+
+const slogan = document.querySelector('.slogan');
+const sloganTitle = document.querySelector('.slogan-title span');
+let flag = false;
+setInterval(function () {
+  // if(!flag && !slogan.classList.contains('change')&&sloganTitle.classList.contains('change')){
+  //   slogan.classList.toggle('change');
+    sloganTitle.classList.toggle('change')
+  // }else {
+  //
+  // }
+  // flag = !flag
+}, 1000);
