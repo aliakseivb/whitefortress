6f60833a9c23efeef9f56e49547dad3d6833dc92
AlexBoronin <alex.boronin.22@gmail.com>
2024-07-29 23:52:44 +0300

    left one font, changed animations, favicon, logo

--- scripts/script.js
+++ scripts/script.js
@@ -2,24 +2,8 @@ new WOW({
   animateClass: 'animate__animated',
 }).init();
 
-// const slogan = document.querySelector('.slogan');
-const sloganTitle = document.querySelector('.slogan-title span');
-
-setInterval(function () {
-  // if(!flag && !slogan.classList.contains('change')&&sloganTitle.classList.contains('change')){
-  //   slogan.classList.toggle('change');
-    sloganTitle.classList.toggle('change')
-  // }else {
-  //
-  // }
-  // flag = !flag
-}, 1000);
-
-const servicesItemsImage = document.querySelectorAll('.services-item-image');
-
-
-const wave = setInterval(function () {
-  Array.from(servicesItemsImage).findIndex(elem => {
-
-  })
-},1000);
+// const sloganTitle = document.querySelector('.slogan-title span');
+//
+// setInterval(function () {
+//     sloganTitle.classList.toggle('change')
+// }, 1000);
