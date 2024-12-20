48aca917e6ea8379f9c6c0dba1d8a3aa9b0ffc8c
AlexBoronin <alex.boronin.22@gmail.com>
2024-12-20 15:37:04 +0300

    feat zoom certificate

--- main.js
+++ main.js
@@ -552,6 +552,8 @@ import certificates from "./src/data/certificates.js";
       body.classList.remove('hidden');
     });
 
+
+    /** СОЗДАЕМ СЛАЙДЕР СЕРТИФИКАТОВ */
     function makeCertificateSlider(certificates){
       const swiperWrapper = document.getElementById('swiperWrapper');
       certificates.forEach(item => {
@@ -559,6 +561,44 @@ import certificates from "./src/data/certificates.js";
       })
     }
     makeCertificateSlider(certificates)
+
+    /** ЛОВИМ КЛИК ПО СЛАЙДУ ДЛЯ ОТКРЫТИЯ ПОПАПА */
+    $('.swiper-slide-image').magnificPopup({
+      type: 'image',
+      mainClass: 'mfp-with-zoom', // this class is for CSS animation below
+      removalDelay: 300,
+      zoom: {
+        enabled: true, // By default it's false, so don't forget to enable it
+
+        duration: 300, // duration of the effect, in milliseconds
+        easing: 'ease-in-out', // CSS transition easing function
+
+        // The "opener" function should return the element from which popup will be zoomed in
+        // and to which popup will be scaled down
+        // By defailt it looks for an image tag:
+        opener: function(openerElement) {
+          // openerElement is the element on which popup was initialized, in this case its <a> tag
+          // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+          return openerElement.is('img') ? openerElement : openerElement.find('img');
+        }
+      }
+      // other options
+    });
+
+    const slider = document.querySelector('.swiper');
+    slider.addEventListener('click', (e) => {
+      const tmp = e.target.getAttribute('style').split(';')[0].split("url('.")[1]
+      const elemToPopup = tmp.slice(0,tmp.length-2)
+
+      $.magnificPopup.open({
+        items: {
+          src: `${elemToPopup}`
+        },
+        type: 'image'
+      });
+    })
+
+
   }
 
   if (location.href.includes('partner.html')) {
